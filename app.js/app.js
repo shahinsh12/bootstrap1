7b0a9b0bb9eab91b0af1b62f3c511088b15951ce
MOHAMMAD SHAHINUL HAQUE <125897285+shahinsh12@users.noreply.github.com>
2023-10-09 01:25:53 +0600

    Add files via upload

---
+++ app.js/app.js
@@ -0,0 +1,35 @@
+$(function(){
+
+
+    $('#banner').slick(
+        {   infinite: true,
+         adaptiveHeight: true,
+         prevArrow:false,
+         nextArrow:false,
+         autoplaySpeed:300,
+         autoplay:true,
+
+
+           
+          
+       
+         
+            
+           
+            
+
+
+                
+          
+
+
+    
+   
+
+
+
+        }
+    );
+
+
+})
